5db062e1c219983962e3c46965dbb1d75e2a4aa2
Chris Moore <35407569+csm123199@users.noreply.github.com>
2021-04-28 14:46:09 +0000

    cmlterm: don't pollute dir on shell completions

--- cmlterm/__cmlterm_completion_wrapper.sh
+++ cmlterm/__cmlterm_completion_wrapper.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Use as:
-#   source __cmlterm_completion_wrapper.sh
-#   declare -F __cmlterm_shell_comp cmlterm
-
 # COMP_CWORD - shell funcs
 # COMP_LINE
 # COMP_POINT
@@ -20,31 +16,37 @@ function _cmlterm() {
 	local exe="$1"
 	local word="$2"
 	local prev_word="$3"
+	
+	# re-export it for the completions binary
+	export COMP_LINE="$COMP_LINE"
+	export COMP_POINT="$COMP_POINT"
+	export COMP_TYPE="$COMP_TYPE"
+	export COMP_KEY="$COMP_KEY"
+
 	local BINNAME="__cmlterm_shell_completion"
 	local binpath="$BINNAME"
+
+	# helpful during development
 	if which "./target/debug/$BINNAME" &>/dev/null ; then
 		binpath="./target/debug/$BINNAME"
 	elif which "../target/debug/$BINNAME" &>/dev/null ; then
 		binpath="../target/debug/$BINNAME"
 	fi # hopefully it's globally available... not much more we can try
 
-	# re-export it
-	export COMP_LINE="$COMP_LINE"
-	export COMP_POINT="$COMP_POINT"
-	export COMP_TYPE="$COMP_TYPE"
-	export COMP_KEY="$COMP_KEY"
-
-	#for i in "${COMP_WORDS[@]}" ; do
-	#	echo -n "$i"
-	#	echo -ne '\0'
-	#done | ../target/debug/__cmlrest_shell_completion --wordbreaks "$COMP_WORDBREAKS" --exe "$exe" --word "$word" --prev-word "$3" | tr '\0' '\n' # read -r -a COMPREPLY -d $'\0'
+	if [ -z "$COMP_DEBUGFILE" ] ; then
+		COMP_DEBUGFILE=/dev/null
+	fi
 
 	readarray -d $'\0' COMPREPLY < <(
 		for i in "${COMP_WORDS[@]}" ; do
 			echo -n "$i"
 			echo -ne '\0'
-		done | "$binpath" --wordbreaks "$COMP_WORDBREAKS" --exe "$exe" --word "$word" --prev-word "$3" 2>debug.txt
+		done | "$binpath" --wordbreaks "$COMP_WORDBREAKS" --exe "$exe" --word "$word" --prev-word "$3" 2>"$COMP_DEBUGFILE"
 	)
+
+	if [[ "${#COMPREPLY[@]}" -eq 0 && -z "$CML_HOST" && -z "$CML_USER" && ( -z "$CML_PASS" || -z "$CML_PASS64") ]] ; then
+		echo -e "\nNo completions found. Are CML_HOST/CML_USER/CML_PASS64 environment variables set?" >&2
+	fi
 }
 
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] || echo "the completions script must be sourced into the shell, not ran as a subshell"
